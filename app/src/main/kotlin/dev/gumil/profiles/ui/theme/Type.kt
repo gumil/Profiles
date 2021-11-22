1e4e266bb9acd45dc4d2a7da34e457c256f38937
Miguel Panelo <miguelrpanelo@gmail.com>
2021-11-22 13:42:17 +0100

    Update typography to use sans pro

--- app/src/main/kotlin/dev/gumil/profiles/ui/theme/Type.kt
+++ app/src/main/kotlin/dev/gumil/profiles/ui/theme/Type.kt
@@ -2,27 +2,35 @@ package dev.gumil.profiles.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.gumil.profiles.R
 
-// Set of Material typography styles to start with
+private val SourceSansPro = FontFamily(
+    Font(R.font.source_sans_pro_regular),
+    Font(R.font.source_sans_pro_semibold, FontWeight.SemiBold),
+    Font(R.font.source_sans_pro_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = SourceSansPro,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 16.sp,
+        letterSpacing = 0.4.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h5 = TextStyle(
+        fontFamily = SourceSansPro,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        letterSpacing = 1.6.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = SourceSansPro,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        letterSpacing = 2.4.sp
     )
-    */
 )
